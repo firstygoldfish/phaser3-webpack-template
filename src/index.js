917a25a0f9d1b57696d44a67a8003173fe8cf1d3
Carl <carl.last@gmail.com>
2023-02-01 00:58:00 +0000

    Changed index.js sprite rotation

--- src/index.js
+++ src/index.js
@@ -22,17 +22,19 @@ function preload() {
 }
 
 let player = null;
+let alien = null;
 
 function create() {
     // x, y, key of image
     this.add.image(config.width / 2, config.height / 2, 'background').setOrigin(0.5,0.5);
-    player = this.add.sprite(config.width / 2, config.height / 2, 'character').setOrigin(0.5,0.5);
-    //player.setScale(0.1, 0.1);
+    player = this.add.sprite(150, 380, 'character').setOrigin(0.5,0.5);
+    alien = this.add.sprite(620, 194, 'character').setOrigin(0.5,0.5);
     //debugger;
 }
 
 function update(time, delta) {
-    
+    player.angle += 1;
+    alien.angle -= 2;
 }
 
 new Phaser.Game(config);
